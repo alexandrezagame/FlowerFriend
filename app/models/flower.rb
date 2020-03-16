317c808a596461a1bda975b7446b0ff1906ce8bb
Felix Rönn <felix.ronn@gmail.com>
2020-03-16 17:31:07 +0100

    models associations and validations

--- app/models/flower.rb
+++ app/models/flower.rb
@@ -1,3 +1,7 @@
 class Flower < ApplicationRecord
+  SPECIES = %w(Rose Cactuc Lily Tulip Pione) #API HERE LATER
   belongs_to :user
+  validates :name, presence: true
+  validates :species, presence: true, inclusion: {in: SPECIES}
+  validates :price_per_day, presence: true
 end
