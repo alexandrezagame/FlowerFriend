2a700bfb80e3d9133844a18f51ae52d9cb1e679e
Felix Rönn <felix.ronn@gmail.com>
2020-03-16 16:46:53 +0100

    generated flower/booking models

---
+++ app/models/flower.rb
@@ -0,0 +1,3 @@
+class Flower < ApplicationRecord
+  belongs_to :user
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_16_153301) do
+ActiveRecord::Schema.define(version: 2020_03_16_154505) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.integer "total_price"
+    t.bigint "user_id"
+    t.bigint "flower_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["flower_id"], name: "index_bookings_on_flower_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "flowers", force: :cascade do |t|
+    t.string "name"
+    t.string "species"
+    t.integer "price_per_day"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_flowers_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,4 +51,7 @@ ActiveRecord::Schema.define(version: 2020_03_16_153301) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "flowers"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "flowers", "users"
 end
