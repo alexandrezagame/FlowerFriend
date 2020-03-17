b43138238dd941e28f6856ada43d7a9bdceab698
Alexandre Zagame <zagamealexandre@gmail.com>
2020-03-17 11:49:39 +0100

    created the new, view,create and params

--- app/controllers/flowers_controller.rb
+++ app/controllers/flowers_controller.rb
@@ -2,4 +2,24 @@ class FlowersController < ApplicationController
   def index
     @flowers = Flower.all
   end
+
+  def create
+    @flower = Flower.new(flower_params)
+
+    if @flower.save
+      redirect_to @flower, notice: 'You flower was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def new
+    @flower = Flower.new
+  end
+
+  private
+
+  def flower_params
+    params.require(:flower).permit(:name, :species)
+  end
 end
