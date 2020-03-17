853277549e4acee7d09457199b043707d82a118a
Felix Rönn <felix.ronn@gmail.com>
2020-03-17 15:32:54 +0100

    delete, form adjustments, update, edit, before actions, create button

--- app/controllers/flowers_controller.rb
+++ app/controllers/flowers_controller.rb
@@ -1,10 +1,11 @@
 class FlowersController < ApplicationController
+  before_action :set_flower, only: [:show, :edit, :update, :destroy]
+
   def index
     @flowers = Flower.all
   end
 
   def show
-   @flower = Flower.find(params[:id])
   end
 
   def create
@@ -17,11 +18,31 @@ class FlowersController < ApplicationController
     end
   end
 
+  def update
+    if @flower.update(flower_params)
+      redirect_to @flower, notice: 'Flower was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def edit
+  end
+
   def new
     @flower = Flower.new
   end
 
+  def destroy
+    @flower.destroy
+    redirect_to flowers_path, notice: "Your flower has been destroyed!"
+  end
+
   private
+
+  def set_flower
+    @flower = Flower.find(params[:id])
+  end
 
   def flower_params
     params.require(:flower).permit(:name, :species)
