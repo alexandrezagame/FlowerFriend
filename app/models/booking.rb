bf0880e6eeee0055dadf022c13a0f964c76e28f4
Felix Rönn <felix.ronn@gmail.com>
2020-03-18 17:50:48 +0100

    booking page, footer removed, frontend

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,6 +3,6 @@ class Booking < ApplicationRecord
   belongs_to :flower
   # validates :total_price, presence: true , need callback fix
   validates :start_date, presence: true
-  validates :end_date, presence: true
+  # validates :end_date, presence: true
   #{scope} ?
 end
