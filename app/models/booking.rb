f1575f0e5ebfb852c9f4afd15b7c41a510d821cf
Alexandre Zagame <zagamealexandre@gmail.com>
2020-03-18 16:52:56 +0100

    added total pricing when creating a flower

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :flower
-  # validates :total_price, presence: true , need callback fix
+  # validates :total_price, presence: true
   validates :start_date, presence: true
   validates :end_date, presence: true
   #{scope} ?
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_03_18_134034) do
-
+ActiveRecord::Schema.define(version: 2020_03_18_153356) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +58,7 @@ ActiveRecord::Schema.define(version: 2020_03_18_134034) do
     t.float "latitude"
     t.float "longitude"
     t.string "address"
+    t.string "total_price"
     t.index ["user_id"], name: "index_flowers_on_user_id"
   end
 
